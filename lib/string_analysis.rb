b14ce7421b642e3c51ad12c7b4c888fe8c64c4f3
Amal Hussein <amalhussein08@gmail.com>
2013-10-03 07:36:54 -0400

    Initial commit

---
+++ lib/string_analysis.rb
@@ -0,0 +1,65 @@
+class String 
+
+	def word_count
+		self.count
+	end
+
+	def self.unique_words
+	end
+
+	def unique_word_count
+	end 
+
+	def word_frequencies
+	end 
+
+end 
+
+
+
+
+
+
+
+# class Foo
+#   def self.bar
+#     puts 'class method'
+#   end
+# end
+
+# Foo.bar # "class method"
+
+# # Way 2
+# class Foo
+#   class << self
+#     def bar
+#       puts 'class method'
+#     end
+#   end
+# end
+
+# Foo.bar # "class method"
+
+# # Way 3
+# class Foo; end
+# def Foo.bar
+#   puts 'class method'
+# end
+
+
+# word_count
+
+# Returns the number of words in a string.
+
+# unique_words
+
+# Returns an array of unique words present in the string.
+
+# unique_word_count
+
+# Returns the number of unique words present in the string.
+
+# word_frequencies
+
+# Returns a hash of the unique words and their frequencies present in the string.
+
